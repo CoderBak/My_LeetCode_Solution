1290bfa4c6594808d55f88806c26e03d99f280fd
CoderBak <hx-sun@outlook.com>
2023-11-30 14:20:10 +0800

    Update

--- 5.最长回文子串.py
+++ 5.最长回文子串.py
@@ -9,12 +9,13 @@ class Solution:
     def longestPalindrome(self, s: str) -> str:
         maxL = 0
         maxS = ""
-        for l in range(s.length - 1):
-            for r in range(l, s.length):
-                sub_str = s[l: r-1]
+        for l in range(len(s)):
+            for r in range(l, len(s)):
+                sub_str = s[l: r+1]
                 rev_str = sub_str[::-1]
                 if sub_str == rev_str:
                     if len(sub_str) > maxL:
                         maxS = sub_str
+                        maxL = len(sub_str)
         return maxS
 # @lc code=end
